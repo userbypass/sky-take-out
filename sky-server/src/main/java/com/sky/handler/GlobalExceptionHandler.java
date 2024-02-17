5fb7b995687bab5f9bef09c2d8925b612a71ce28
noir <1984776149@qq.com>
2024-02-17 15:55:39 +0800

    增加userName重复的异常的处理机制

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
@@ -23,5 +25,17 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
-
+    /**
+     * @Description 处理userName重复导致的异常
+     * @Date 2024/2/17 15:53
+     * @Param [ex]
+     * @return com.sky.result.Result
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        // userName已使用
+        String duplicateName = ex.getMessage().split(" ")[2];
+        String msg = duplicateName.substring(1,duplicateName.length()-1)+"已存在，请重新输入";
+        return Result.error(msg);
+    }
 }
